df6c658ea8c3e9cd8146d8fd01509f58ec4a8a3c
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2021-10-08 10:51:58 -0500

    Completed the js with html lec

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,28 @@
+
+
+"use strict";
+var aida ="aida";
+console.log(aida);
+
+
+console.log("Hello, I am in my own file now!");
+
+var returnOfAlert = alert("TGIF! Thank Goodness I'm Fine");
+
+console.log(returnOfAlert);
+
+var returnOfconfirm= confirm("Got any plans for this three day weekend?");
+console.log(returnOfconfirm);
+
+
+var favoriteNuMetalBand = prompt("What is your favorite nu-metal band?");
+console.log("Ha, ha! you like " + favoriteNuMetalBand " !");
+
+
+var leastfavoriteSoda = prompt("what is your least favorite soda?");
+var favoriteSoda = prompt("What is your favorite soda?");
+
+alert("Hello fello teens, my name is " + leastfavoriteSoda + " " + favoriteSoda + " and I choose hugs not drugs.");
+
+
+
