1f3cb8ccf124b5bf230a6808e36f4da92d2c78e0
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2021-10-18 19:37:24 -0500

    Finished loops, except do-while

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,28 +1,28 @@
-(function(){
+(function() {
     //1. Create a file named break_and_continue.js in the js directory.
     // 2. Prompt the user for an odd number between 1 and 50. Use a loop
     // and a break statement to continue prompting the user if they enter invalid input.
     // 3. Use a loop and the continue statement to output all the odd numbers between
-    //      1 and 50, except for the number the user entered.
 
+//      1 and 50, except for the number the user entered.
 
+    let userOdd = prompt("Please, choose an odd number between 1 and 50");
+    while (true) {
 
-    // 4.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (userOdd % 2 === 0) {
+            alert(" That number is not odd, please enter an odd number.");
+            continue;
+        } else {
+            break;
+        }
+    }
+    for (var i = 0; i <= 50; i++) {
+        if ((i % 2) != 0) {
+            if (i == userOdd) {
+                continue;
+            }
+            console.log(i);
+        }
+    }
 
 })();
--- js/while.js
+++ js/while.js
@@ -2,10 +2,11 @@
 //1. Create a file named while.js in the js directory.
 //
 //2.  Create a while loop that uses console.log() to create the output shown below.
-
-
-
-
+i = 2;
+    while (i <= 65536) {
+        console.log(i);
+        i *= 2;
+    }
 
 // Do While Loop
 // An ice cream seller can't go home until she sells all of her cones.
@@ -15,5 +16,27 @@
 // between 1 and 5, simulating the amount of cones being bought by her clients.
 // Use a do-while loop to log to the console the amount of cones sold
 // to each person. This is a way get the random numbers for this exercise.
+
+    var conesToSell = Math.floor(Math.random() * (100 - 50 + 1) + 50);
+        do {
+            var conesBoughtByCostumers = Math.floor(Math.random() * (5 - 1+ 1) + 1);
+            console.log(conesBoughtByCostumers);
+            conesBoughtByCostumers --;
+        }while (conesToSell > 50);
+
+
+    console.log();
+
+
+
+
+
+
+
+
+
+
+
+
 
 })();
