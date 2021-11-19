fb498847f360d434e9912575cdd5e7f32ef3be91
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2021-11-19 15:08:49 -0600

    JQ lecture

---
+++ js/jquery_exercises.js
@@ -0,0 +1,54 @@
+
+//ID selectors
+
+//alert($('#info').html());
+//OR
+// var title= $("#info").html()
+// alert(title);
+//alert($('#info-title').html());
+// var title= $("#info-title").html()
+// alert(title);
+//$("#header").css("color","blue");
+
+
+
+//Class selectors
+// $(".codeup").css('border', '1px solid red');
+
+
+// element selectors
+$("li").css("font-size", "20px");
+$('li');
+$('h1');
+$('p');
+
+alert($("h1").html());
+
+
+//Multiple selectors
+
+$('h1','p', 'li');
+
+
+
+//jq events
+//
+//
+// $(".one-btn").click(function(){
+// $("h1").css("color","#49FF00");
+// });
+// //
+// $("p").on("mouseover", function() {
+//     $("p").css("color","#FF0075");
+// });
+// $("h1").click(function(){
+//     $("h1").css("background-color", "red");
+// });
+//
+// $("p").dblclick( function(){
+//    $("p").css("font-size", "18px") ;
+// });
+//
+// $("li").hover( function() {
+//     $("li").css("color","red");
+// });
