4431447492f413075b56d3de3fa5e84ea8bfd268
aidaj gutierrez <aidaj.gutierrez@gmail.com>
2021-12-09 17:01:14 -0600

    added bonus with David's answer

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,5 @@
-//
-// fetch('https://api.github.com/users/AidaJ-Gutierrez/events', {headers: {'Authorization':GITHUB_KEY}})
+(() =>{
+    // fetch('https://api.github.com/users/AidaJ-Gutierrez/events', {headers: {'Authorization':GITHUB_KEY}})
 //     .then(function (results) {
 //         console.log(results);
 //         return results.json()
@@ -11,32 +11,52 @@
 //         console.log( date)
 //     });
 
-function recentCommit(username){
+    function recentCommit(username){
 
-    fetch('https://api.github.com/users/' + username + '/events', {headers: {'Authorization':GITHUB_KEY}})
-        .then(function (results) {
-            console.log(results);
-            return results.json()
-        })
-        .then(function (resultsArray) {
-            console.log(resultsArray);
-            var date = resultsArray[0].created_at;
-            console.log(username + ' last commit was : ' + date)
+        fetch('https://api.github.com/users/' + username + '/events', {headers: {'Authorization':GITHUB_KEY}})
+            .then(function (results) {
+                console.log(results);
+                return results.json()
+            })
+            .then(function (resultsArray) {
+                console.log(resultsArray);
+                var date = resultsArray[0].created_at;
+                console.log(username + ' last commit was : ' + date)
 
+            });
+
+    }
+    console.log(recentCommit('AidaJ-Gutierrez'));
+
+    //
+    // const wait = (milliseconds) => {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve();
+    //         }, milliseconds)
+    //     });
+    // };
+    //
+    //
+    // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+    // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+///ANSWER WITH DAVID'S BONUS EXPLAINED
+    const wait = (milliseconds) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(`You'll see this after ${milliseconds/1000} second(s).`);
+            }, milliseconds)
         });
-
-}
-console.log(recentCommit('AidaJ-Gutierrez'));
+    };
 
 
-const wait = (milliseconds) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, milliseconds)
-    });
-};
+    wait(1000).then(message => console.log(message))
+    wait(3000).then(message => console.log(message))
 
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+})();
+
